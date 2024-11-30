2b36adbada0834dc39e01e5c4a223c29a0149df3
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-11-29 22:31:32 -0800

    made changes

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 mkdir prism
 mkdir prism/boot
 mkdir prism/boot/grub
@@ -7,6 +8,7 @@ mkdir assembly_line/packages
 mkdir assembly_line/bsd
 mkdir assembly_line/elf_bin
 touch prism/boot/grub/grub.cfg
+
 cat >/prism/boot/grub/grub.cfg <<EOL
 set default=0
 set timeout=0
@@ -16,11 +18,13 @@ menuentry "PRISM OS" {
 }
 ... 
 EOL
+
 git clone https://git.freebsd.org/src.git
 git clone https://github.com/alpinelinux/apk-tools
 git clone https://github.com/mirror/busybox
 git clone https://github.com/bminor/musl
 mv src freebsd-src
+
 cd freebsd-src
 ./build.sh
 ./config
@@ -29,29 +33,38 @@ make kernel
 make installworld DESTDIR=../assembly_line/bsd
 make installkernel DESTDIR=../assembly_line/bsd
 cd ..
+
 cd apk-tools
 meson setup -Dc_link_args="-static" -Dprefer_static=true -Ddefault_library=static build
 ninja -C build src/apk
 mv build/src/apk ../assembly_line/packages/apk
 cd ..
+
 cd busybox
 make
 mv *.ko ../assembly_line/packages/
 cd ..
+
 cd musl
 make *.so ../assembly_line/packages/
 cd ..
+
 for file in ./assembly_line/packages*.so; do
     objcopy --input binary --output elf64-x86-64 --binary-architecture i386:x86-64 $file.so $file.elf
 done
+
 for file in ./assembly_line/packages*.ko; do
     mv $file.ko $file.elf
 done
+
 for file in ./assembly_line/packages*.o; do
     mv $file.o $file.elf
 done
+
 ld -r ./assembly_line/packages/*.elf -o ./assembly_line/elf_bin/packages.elf
+
 grub-mkrescue -o prism.iso prism/
+
 rm -rf prism
 rm -rf freebsd-src
 rm -rf apk-tools
