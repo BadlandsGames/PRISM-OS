b752dd67c9fc42ddcbaf9d4f0a16e70fd1b6ffdc
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-11-30 15:41:11 -0800

    made changes

--- html_view.cu
+++ html_view.cu
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <array>
 #include <libxml/HTMLparser.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 using namespace std;
+
+string exec(const char* cmd) {
+    array<char, 128> buffer;
+    string result;
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if(!pipe) {
+        throw runtime_error("popen() failed!");
+    }
+    while(fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    {
+        result += buffer.data();
+    }
+    return result; 
+}
 
 string parseHTML(const char* filename) {
     LIBXML_TEST_VERSION
