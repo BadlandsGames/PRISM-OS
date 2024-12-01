d9ba249cb3b087b8e1898dbef179690258d8bdae
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-11-30 18:23:05 -0800

    made changes

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ mkdir assembly_line/elf_bin
 touch prism/boot/grub/grub.cfg
 touch prism/boot/init.sh
 touch prism/boot/startup.ps1
+touch prism/boot/api.ps1
 
 cat >/prism/boot/grub/grub.cfg <<EOL
 set default=0
@@ -59,6 +60,33 @@ cat >prism/boot/startup.ps1 <<EOL
 #!/usr/bin/powershell
 
 shutdown -h now
+
+...
+EOL
+
+cat >prism/boot/api.ps1 <<EOL
+#!/usr/bin/powershell
+
+function RunExeViaProton {
+    param (
+        [string]$File
+    )
+    eval "$PROTON_FOLDER_PRISM/proton run /fs/$File"
+}
+
+function RunExeViaWine {
+    param (
+        [string]$File
+    )
+    eval "wine /fs/$File"
+}
+
+function RunExeViaWineMono {
+    param (
+        [string]$File
+    )
+    eval "wine-mono /fs/$File"
+}
 
 ...
 EOL
