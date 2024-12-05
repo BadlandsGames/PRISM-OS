cc50cf93152bf7403238125796aa4489076cffb5
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-05 15:07:26 -0800

    made changes

--- bin/prism/prism.c
+++ bin/prism/prism.c
@@ -3,3 +3,33 @@ apk update && pkg update && apt update
 ubuntu-drivers autoinstall
 apk update && pkg update && apt update
 */
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef SHELL
+#define main killcmd
+#include "bltin/bltin.h"
+#endif
+
+static void usage(void) __dead2;
+
+int main(int argc, char *argv[]) {
+}
+
+static void usage(void) {
+	(void)fprintf(stderr, "%s\n%s\n%s\n%s\n",
+		"usage: kill [-s signal_name] pid ...",
+		"       kill -l [exit_status]",
+		"       kill -signal_name pid ...",
+		"       kill -signal_number pid ...");
+#ifdef SHELL
+	error(NULL);
+#else
+	exit(2);
+#endif
+}
