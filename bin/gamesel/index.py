90fd80e5ed5c7c89a8cbb4cf30db63001a469331
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-06 15:24:55 -0800

    made changes

--- bin/gamesel/index.py
+++ bin/gamesel/index.py
@@ -1,14 +1,29 @@
 from simple_term_menu import TerminalMenu
 import os
+import subprocess
+import json
+
+chosen_index = ""
 
 def menuloop(folder):
     files_list = []
     for root, directories, files in os.walk(folder):
         for name in files:
-            files_list.append(os.path.join(root, name))
+            process = subprocess.Popen(
+                ['unzip', '-q', '-p', os.path.join(root, name), 'tempname.txt', '|', 'cat'],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True)
+            stdout = json.loads(process.communicate())["name"]
+            files_list.append(stdout + ", " + os.path.join(root, name))
+            os.system("rm tempname.txt")
     terminal_menu = TerminalMenu(files_list)
     menu_entry_index = terminal_menu.show()
-    print(f"You have selected {options[menu_entry_index]}!")
+    chosen_index = str(files_list[menu_entry_index])
 
 def main(folder):
-    menuloop(folder)
+    newfldr = folder
+    menuloop(newfldr)
+    if newfldr.endswith("/"):
+        newfldr = newfldr[:-1]
+    os.system("psmapp --execute " + newfldr + "/" + chosen_index)
