8a9acc1f14383be8bd0c9b96688d6eef73464fa2
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-05 20:03:48 -0800

    made changes

---
+++ bin/psmterm/psmterm.cpp
@@ -0,0 +1,51 @@
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <sys/proc.h>
+#include <sys/queue.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/user.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <paths.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <regex.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <kvm.h>
+#include <err.h>
+#include <pwd.h>
+#include <grp.h>
+#include <errno.h>
+#include <locale.h>
+#include <jail.h>
+
+#ifdef SHELL
+#define main killcmd
+#include "bltin/bltin.h"
+#endif
+
+int main(int argc, char *argv[]) {
+    string newcmd = "sudo pwsh";
+    if(!strcmp(*argv, "--script")) {
+		argc--, argv++;
+		if (argc < 1) {
+			system(newcmd);
+		}
+        if(strcmp(*argv, "0")) {
+            newcmd.append(" ");
+            newcmd.append(*argv);
+			system(newcmd);
+		}
+        argc--, argv++;
+    } else {
+        system(newcmd);
+    }
+    return 0;
+}
