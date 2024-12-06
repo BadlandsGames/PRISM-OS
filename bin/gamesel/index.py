2f7881d7cfbcac01c4807426e8288794101cd754
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-06 15:11:46 -0800

    made changes

--- bin/gamesel/index.py
+++ bin/gamesel/index.py
@@ -1,10 +1,14 @@
 from simple_term_menu import TerminalMenu
+import os
 
-def menuloop():
-    options = ["entry 1", "entry 2", "entry 3"]
-    terminal_menu = TerminalMenu(options)
+def menuloop(folder):
+    files_list = []
+    for root, directories, files in os.walk(folder):
+        for name in files:
+            files_list.append(os.path.join(root, name))
+    terminal_menu = TerminalMenu(files_list)
     menu_entry_index = terminal_menu.show()
     print(f"You have selected {options[menu_entry_index]}!")
 
-def main():
-    menuloop()
+def main(folder):
+    menuloop(folder)
