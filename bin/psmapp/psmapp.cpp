999da7990808428fc791c7ec03ed4338580d99d9
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-06 14:51:36 -0800

    made changes

--- bin/psmapp/psmapp.cpp
+++ bin/psmapp/psmapp.cpp
@@ -1,5 +1,46 @@
-extern
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <sys/proc.h>
+#include <sys/queue.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/user.h>
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <paths.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <regex.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <kvm.h>
+#include <err.h>
+#include <pwd.h>
+#include <grp.h>
+#include <errno.h>
+#include <locale.h>
+#include <jail.h>
+
+#include <iostream>
+using namespace std;
+
+extern void run_psmapp(string filename_in);
 
 #ifndef __USE_INDEX_ELF__
 #include "index.h"
 #endif
+
+#ifdef SHELL
+#define main killcmd
+#include "bltin/bltin.h"
+#endif
+
+#define __USE_INDEX_ELF__
+
+int main(int argc, char *argv[]) {}
